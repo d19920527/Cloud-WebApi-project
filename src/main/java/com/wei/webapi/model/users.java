f2ed333a83458a9519aba4aaba1c1fa2b1968653
d19920527 <d362514@gmail.com>
2025-06-15 23:23:48 +0800

    更正

--- src/main/java/com/wei/webapi/model/users.java
+++ src/main/java/com/wei/webapi/model/users.java
@@ -17,8 +17,13 @@ public class users {
 		private String name;
 		private String email;
 		private String username;
+<<<<<<< HEAD
 		private String userid;
 		private int tree;
+=======
+		private int userid;
+		private String usernamepassword;
+>>>>>>> aa73843 (新增usernamepassword)
 		public int getId() {
 			return id;
 		}
