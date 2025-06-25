a0b88251c592cfcefa0f89601846d5537128f519
d19920527 <d362514@gmail.com>
2025-06-25 20:21:41 +0800

    tickets this a  Class add  Fields  statuscode

--- src/main/java/com/wei/webapi/model/tickets.java
+++ src/main/java/com/wei/webapi/model/tickets.java
@@ -20,35 +20,54 @@ public class tickets {
 	private String movie_uuid;
 	private String seat;
 	private Date booked_at;
+	private int statuscode;
+
 	public int getId() {
 		return id;
 	}
+
 	public void setId(int id) {
 		this.id = id;
 	}
+
 	public String getUser_uuid() {
 		return user_uuid;
 	}
+
 	public void setUser_uuid(String user_uuid) {
 		this.user_uuid = user_uuid;
 	}
+
 	public String getMovie_uuid() {
 		return movie_uuid;
 	}
+
 	public void setMovie_uuid(String movie_uuid) {
 		this.movie_uuid = movie_uuid;
 	}
+
 	public String getSeat() {
 		return seat;
 	}
+
 	public void setSeat(String seat) {
 		this.seat = seat;
 	}
+
 	public Date getBooked_at() {
 		return booked_at;
 	}
+
 	public void setBooked_at(Date booked_at) {
 		this.booked_at = booked_at;
+	}
+
+	public int getStatuscode() {
+		return statuscode;
+	}
+
+	public void setStatuscode(int statuscode) {
+		this.statuscode = statuscode;
 	}
 
 }
